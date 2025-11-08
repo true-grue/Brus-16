ff332f6128ca78093611a92393b5c85e65cce1d7
Peter Sovietov <peter.sovietov@gmail.com>
2025-11-08 07:46:20 +0300

    pushu/push is better

--- brus16_cpu.c
+++ brus16_cpu.c
@@ -131,12 +131,12 @@ uint16_t exec_f2(struct CPU *cpu, uint16_t val, uint16_t new_pc) {
 void step(struct CPU *cpu) {
     if (!cpu->wait) {
         uint16_t val = cpu->code[cpu->pc];
-        uint16_t new_pc = (cpu->pc + 1) & (CODE_SIZE - 1);
+        uint16_t new_pc = cpu->pc + 1;
         if (get_field(val, F_POS, F_SIZE)) {
             new_pc = exec_f1(cpu, val, new_pc);
         } else {
             new_pc = exec_f2(cpu, val, new_pc);
         }
-        cpu->pc = new_pc;
+        cpu->pc = new_pc & (CODE_SIZE - 1);
     }
 }
